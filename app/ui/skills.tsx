9ec9a31cf660d9a092a71a752fe14991b8e81280
dianaguerrero <dguerreropacheco@gmail.com>
2026-05-28 19:47:39 -0500

    bd-skills

--- app/ui/skills.tsx
+++ app/ui/skills.tsx
@@ -1,11 +1,29 @@
-export default function Skills() {
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma";
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export default async function Skills() {
+  const listaSkills = await prisma.skill.findMany({
+
+    where: {
+      categoria: 'LENGUAJE',
+
+    }
+   });
+
   return (
     <div className="absolute right-4 md:right-21 top-1/2 translate-y-[-97px]">
       <div className="flex flex-col items-end gap-4">
-        <p className="text">NEXTJS</p>
-        <p className="text">NODEJS</p>
-        <p className="text">PHP</p>
-        <p className="text">PYTHON</p>
+        {listaSkills.length === 0 ? (
+          <p className="text">Sin datos</p>
+        ) : (
+          listaSkills.map((skill) => (
+            <p key={skill.id} className="text">
+              {skill.nombre.toUpperCase()}
+            </p>
+          ))
+        )}
       </div>
     </div>
   );
